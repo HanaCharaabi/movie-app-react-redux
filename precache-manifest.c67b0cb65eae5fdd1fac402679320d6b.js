6c771693697b506f5e38f06fd893880931633da8
HanaCharaabi <charaabihana90@gmail.com>
2019-06-03 12:41:55 +0100

    Updates

---
+++ precache-manifest.c67b0cb65eae5fdd1fac402679320d6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab34f977689d590c47fee7c81848cd5",
+    "url": "/movie-app-react-redux/index.html"
+  },
+  {
+    "revision": "01c49ffe679af8ebbdc5",
+    "url": "/movie-app-react-redux/static/css/main.6dc1021e.chunk.css"
+  },
+  {
+    "revision": "624a95f905f41d6109f3",
+    "url": "/movie-app-react-redux/static/js/2.bf2bf66d.chunk.js"
+  },
+  {
+    "revision": "01c49ffe679af8ebbdc5",
+    "url": "/movie-app-react-redux/static/js/main.5da3517f.chunk.js"
+  },
+  {
+    "revision": "6c71e044f647f310dfed",
+    "url": "/movie-app-react-redux/static/js/runtime~main.cf937925.js"
+  }
+]);
